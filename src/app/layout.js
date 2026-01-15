b34d3c33fc98cb0a121b8400db9d34baa359e79d
AaminSPatel <locomail112@gmail.com>
2026-01-15 13:06:16 +0530

    inhanced seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,9 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico',rel: 'icon' },
+      { url: '/favicon.png', sizes: '48x48', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/favicon.svg', type: 'image/svg+xml' },
     ],
